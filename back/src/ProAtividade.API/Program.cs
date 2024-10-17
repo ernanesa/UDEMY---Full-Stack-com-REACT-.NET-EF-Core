a5141fc5681f2d61b7fde51f6eb7148b3125fcfa
Ernane Sa <ernane@itera.com.br>
2024-10-17 09:15:48 -0300

    create a project

---
+++ back/src/ProAtividade.API/Program.cs
@@ -0,0 +1,41 @@
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using ProAtividade.API.Data;
+using ProAtividade.API.Interfaces;
+using ProAtividade.API.Repositories;
+using ProAtividade.API.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? "Data Source=ProAtividade.db"));
+
+builder.Services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.IgnoreNullValues = true;
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                });
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IAtividadeRepository, AtividadeRepository>();
+builder.Services.AddScoped<IAtividadeService, AtividadeService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+app.Run();
